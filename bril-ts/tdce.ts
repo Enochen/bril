72bacd56d8e760370351852a054a05bba0e138df
Enochen <sirenochen@gmail.com>
2023-09-05 19:50:36 -0400

    trivial dead code elimination in typescript

---
+++ bril-ts/tdce.ts
@@ -0,0 +1,50 @@
+import * as bril from './bril.ts';
+import * as iter from 'https://deno.land/x/iter@v3.1.0/mod.ts';
+import { Line, formBlocks } from './form_blocks.ts';
+import { readStdin } from './util.ts';
+
+// Trivial form of dead code elimination, returns true if instructions changed
+function eliminateDeadCode(func: bril.Function): boolean {
+  const blocks = [...formBlocks(func)];
+
+  // Track things we cannot delete
+  const used = new Set<string>();
+  for (const block of blocks) {
+    for (const instr of block) {
+      if ('args' in instr) {
+        instr.args?.forEach((arg) => used.add(arg));
+      }
+    }
+  }
+
+  let dirty = false;
+
+  function* cleanBlock(block: Iterable<Line>) {
+    for (const instr of block) {
+      if ('dest' in instr && !used.has(instr.dest)) {
+        dirty = true;
+        continue;
+      }
+      yield instr;
+    }
+  }
+
+  const newInstrs = iter.flatMap(blocks, cleanBlock);
+  func.instrs = [...newInstrs];
+
+  return dirty;
+}
+
+function optimizeLocal(func: bril.Function) {
+  while (eliminateDeadCode(func)) {
+    // iterate to convergence!
+  }
+}
+
+if (import.meta.main) {
+  const program = JSON.parse(await readStdin()) as bril.Program;
+  program.functions.forEach(optimizeLocal);
+
+  const json = JSON.stringify(program, undefined, 2);
+  await Deno.stdout.write(new TextEncoder().encode(json));
+}
